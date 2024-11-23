54c4f6b713028bda7dc4e86fdad94217fbeaea81
ziliolu <luzilio.p@gmail.com>
2024-11-23 13:45:56 -0500

    frontend without django project resetup

---
+++ srcs/frontend/js/routing.js
@@ -0,0 +1,52 @@
+//funcao q e chamada quando a pagina html carrega 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    routing();
+    console.log("[routing.js] DOMContentLoaded");
+});
+
+
+function routing() {
+    // Função que carrega o conteúdo HTML dentro de #app baseado no endpoint
+    console.log("[routing.js] script loaded");
+
+    function loadContentBasedOnUrl() {
+        // Pega a URL atual
+        const url = window.location.pathname;
+        console.log(`[routing.js] URL: ${url}`);
+
+        // Verifica se a URL é a raiz
+        if (url === "/") {
+            // Carrega o conteúdo da página inicial
+            loadContent("login.html");
+        } else {
+            // Carrega o conteúdo baseado na URL
+            const endpoint = url.replace("/", "");
+            loadContent(endpoint);
+        }
+    }
+
+    loadContentBasedOnUrl();
+};
+
+function loadContent(endpoint) {
+    // Pega o elemento #app
+    const app = document.getElementById("app");
+
+    // Faz a requisição fetch para o endpoint dinâmico
+    fetch(endpoint)
+        .then(response => {
+            if (response.ok) {
+                return response.text();  // Se a resposta for ok, trata como texto (HTML)
+            }
+            throw new Error('Erro ao carregar o conteúdo');
+        })
+        .then(html => {
+            // Insere o conteúdo HTML dentro de #app
+            app.innerHTML = html;
+        })
+        .catch(error => {
+            console.error(`[routing.js] ${error}`);
+        });
+}
