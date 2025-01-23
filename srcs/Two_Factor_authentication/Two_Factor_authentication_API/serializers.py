53c927fc5f03176fb10b9d7335b8f7968cdd5893
Xeigar <deaththekid32@hotmail.com>
2025-01-23 23:30:13 +0000

    Api merger

--- srcs/Two_Factor_authentication/Two_Factor_authentication_API/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UserLoginAttempt
-
-
-class UserLoginAttemptSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserLoginAttempt
-        fields = ['user', 'attempts', 'last_failed_attempt', 'last_sucessful_attempt']
--- srcs/Two_Factor_authentication/Two_Factor_authentication_API/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import QRGenView, QRVerView
-
-urlpatterns = [
-    path('gen/', QRGenView.as_view(), name='generate_QR'),
-    path('verify/', QRVerView.as_view(), name='verify_code'),
-]
-
