37198bb66d14f3a8eed82b6bd73063605f31e300
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2025-02-04 12:32:43 +0000

    Add AddFriendView

--- srcs/user_management_service/users/urls.py
+++ srcs/user_management_service/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, getUserView
+from .views import UserProfileViewSet, AddFriendView
 
 # # The router is responsible for generating automatically the urls using the viewSet
 router = DefaultRouter()
 router.register('', UserProfileViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('add-friends', AddFriendView.as_view(), name='add-firend'),
 ]
