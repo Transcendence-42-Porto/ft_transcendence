979595ace0465340a2248e07933faad8dcfe252d
Luiza Zilio <luzilio.p@gmail.com>
2025-03-13 14:06:02 +0000

    fix in entrypoint

--- srcs/user_management_service/entrypoint.sh
+++ srcs/user_management_service/entrypoint.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
 set -e
+sleep 7 
 
-# Run database migrations and run server
-poetry run python manage.py makemigrations --noinput
-poetry run python manage.py migrate --noinput
+echo "Waiting for PostgreSQL to start..."
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "Waiting for PostgreSQL..."
+  sleep 2
+done
+echo "PostgreSQL is ready!"
 
-# Create superuser if not already exists (optional)
+echo "Applying migrations..."
+retry_count=0
+max_retries=5
+until poetry run python manage.py migrate --noinput; do
+    ((retry_count++))
+    if [ $retry_count -ge $max_retries ]; then
+        echo "Migration failed after $max_retries attempts. Exiting."
+        exit 1
+    fi
+    echo "Migration failed. Retrying in 5 seconds..."
+    sleep 5
+done
+
+poetry run python manage.py migrate --fake-initial --noinput
+
+
+echo "Checking for superuser..."
 poetry run python manage.py shell <<END
+import os
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-username = '${DJANGO_ROOT_USER}'
-email = '${DJANGO_ROOT_EMAIL}'
-password = '${DJANGO_SUPERUSER_PASSWORD}'
+username = os.getenv('DJANGO_ROOT_USER', 'admin')
+email = os.getenv('DJANGO_ROOT_EMAIL', 'admin@example.com')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
 if not User.objects.filter(username=username).exists():
-    print(f"Creating superuser {username}")
+    print(f"Creating superuser {username}...")
     User.objects.create_superuser(username=username, email=email, password=password)
 else:
-    print(f"Superuser {username} already exists")
+    print(f"Superuser {username} already exists.")
 END
 
-poetry run python manage.py runserver 0.0.0.0:8000
+echo "Starting Django server..."
+exec poetry run python manage.py runserver 0.0.0.0:8000
