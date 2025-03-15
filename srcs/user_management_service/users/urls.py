4a1cb58e6e42b17ceca7f75c0b7fa1293fcf54fe
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2025-03-15 18:18:42 +0000

    Added upload_image view

--- srcs/user_management_service/users/urls.py
+++ srcs/user_management_service/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, AddFriendView, DeleteFriendView
+from .views import UserProfileViewSet, AddFriendView, DeleteFriendView, UploadImageView
 
 # # The router is responsible for generating automatically the urls using the viewSet
 router = DefaultRouter()
@@ -9,5 +9,6 @@ router.register('', UserProfileViewSet, basename='user')
 urlpatterns = [
     path('', include(router.urls)),
     path('add-friends', AddFriendView.as_view(), name='add-firend'),
-    path('remove-friends', DeleteFriendView.as_view(), name='remove-friend')
+    path('remove-friends', DeleteFriendView.as_view(), name='remove-friend'),
+    path('upload-image', UploadImageView.as_view(), name='upload_image'),
 ]
