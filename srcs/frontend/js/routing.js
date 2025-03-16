531d4f351ff9938c5f909dd5faa5ec7c5554ca93
Sopa de Galinha <jhogonca@c2r5s5.42porto.com>
2025-03-16 10:20:26 +0000

    arrows back and foward

--- srcs/frontend/js/routing.js
+++ srcs/frontend/js/routing.js
@@ -1,59 +1,49 @@
 function routing() {
-    loadContentBasedOnUrl();
-};
+  loadContentBasedOnUrl();
 
-function loadContentBasedOnUrl() {
-    const url = window.location.pathname;
-    
-    if (url === "/") {
-        loadContent("login");
-    } else {
-        const endpoint = url.replace("/", "");
-        loadContent(endpoint);
-    }
+  // Add a listener for the popstate event
+  window.addEventListener("popstate", () => {
+    loadContentBasedOnUrl();
+  });
 }
 
-function loadContent(endpoint) {
-    const app = document.getElementById("app");
+function loadContentBasedOnUrl() {
+  const url = window.location.pathname;
+  const endpoint = url === "/" ? "login" : url.replace("/", "");
+  loadContent(endpoint, false);
+}
 
-    fetch(`/html/${endpoint}.html`)
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            }
-            throw new Error('Erro ao carregar o conteúdo');
-        })
-        .then(html => {
-            app.innerHTML = html;
+function loadContent(endpoint, pushState = true) {
+  const app = document.getElementById("app");
 
-            if(endpoint == "profile")
-            {
-                history.pushState({}, '', '/profile');
-                loadProfile();
-                loadEditProfile();
-            }
-            if(endpoint == "menu")
-            {
-                history.pushState({}, '', '/menu');
-                loadMenu();
-            }
-            if(endpoint == "login")
-            {
-                history.pushState({}, '', '/login');
-            }
-            if(endpoint == "signup")
-            {
-                history.pushState({}, '', '/signup');
-            }
-            if(endpoint == "game")
-              {
-                  history.pushState({}, '', '/game');
-                  initializeGameForm();
-                  loadGameMenu();
-              }
-        })
-        .catch(error => {
-            history.pushState({}, '', '/login');
-            loadContent('login');
-        });
+  fetch(`/html/${endpoint}.html`)
+    .then((response) => {
+      if (response.ok) {
+        return response.text();
+      }
+      throw new Error("Error loading content");
+    })
+    .then((html) => {
+      app.innerHTML = html;
+
+      if (pushState) {
+        history.pushState({}, "", `/${endpoint}`);
+      }
+
+      if (endpoint == "profile") {
+        loadProfile();
+        loadEditProfile();
+      }
+      if (endpoint == "menu") {
+        loadMenu();
+      }
+      if (endpoint == "game") {
+        initializeGameForm();
+        loadGameMenu();
+      }
+    })
+    .catch(() => {
+      history.pushState({}, "", "/login");
+      loadContent("login", false);
+    });
 }
